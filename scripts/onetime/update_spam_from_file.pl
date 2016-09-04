9c17fe814b6cf3bb2e7f1579ebc2123423fa8688
y_uuki <yuki.tsubo@gmail.com>
2016-09-04 19:46:13 +0900

    spamfileからspamテーブルへ書き出す

---
+++ scripts/onetime/update_spam_from_file.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+use utf8;
+use strict;
+use warnings;
+
+my $logfile = $ARGV[0] or die "log file reuqire";
+
+open(my $logfh, '<', $logfile);
+
+my $web = Isuda::Web->new;
+
+while (my $line = readline $logfh) {
+    chomp($line)
+
+    $web->dbh->query(qq[
+        INSERT IGNORE INTO spam (content_hash, valid)
+        VALUES  (?, ?)
+    ], $line, 0);
+}
+
+close($logfh);
