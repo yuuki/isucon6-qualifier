75964194a06a664b113ba9ec066df3a734611f9e
y_uuki <yuki.tsubo@gmail.com>
2016-09-04 18:52:54 +0900

    spamテーブル作る

---
+++ scripts/onetime/update_spam.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+
+use Isuda::Web;
+
+my $web = Isuda::Web->new;
+my $entries = $web->dbh->select_all(qq[
+    SELECT keyword,description FROM entry
+]);
+
+foreach my $entry (@$entries) {
+    my $is_description_valid = $web->is_spam_contents($c, $entry->{description});
+    $web->dbh->query(qq[
+        INSERT INTO spam (content, valid)
+        VALUES  (?, ?)
+    ], $entry->{description}, $is_description_valid);
+
+    my $is_keyword_valid = $web->is_spam_contents($c, $entry->{keyword});
+    $web->dbh->query(qq[
+        INSERT INTO spam (content, valid)
+        VALUES  (?, ?)
+    ], $entry->{keyword}, $is_keyword_valid);
+}
+
