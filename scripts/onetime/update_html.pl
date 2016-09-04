ceb57bff430ee8c799d70ac684ee51fb2ede0f91
y_uuki <yuki.tsubo@gmail.com>
2016-09-04 16:21:59 +0900

    がなかった

--- scripts/onetime/update_html.pl
+++ scripts/onetime/update_html.pl
@@ -3,12 +3,16 @@ use strict;
 use warnings;
 use utf8;
 
+use Kossy::Connection;
 use Isuda::Web;
 
 my $web = Isuda::Web->new;
 my $entries = $web->dbh->select_all(qq[
     SELECT * FROM entry
 ]);
+
+my $c = Kossy::Connection->new;
+
 foreach my $entry (@$entries) {
     my $html = $web->htmlify($web, $entry->{description});
     $web->dbh->query(qq[
