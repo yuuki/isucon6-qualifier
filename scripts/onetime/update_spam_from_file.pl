975798d8a1d1f66902287dcdc410f26f00a69586
y_uuki <yuki.tsubo@gmail.com>
2016-09-04 20:06:06 +0900

    一旦origで

--- scripts/onetime/update_spam_from_file.pl
+++ scripts/onetime/update_spam_from_file.pl
@@ -13,11 +13,11 @@ my $web = Isuda::Web->new;
 
 while (my $line = readline $logfh) {
     chomp($line);
-
+    my $is_description_valid = $web->is_spam_contents($line);
     $web->dbh->query(qq[
         INSERT IGNORE INTO spam (content_hash, valid)
         VALUES  (?, ?)
-    ], $line, 0);
+    ], $line, 0+$is_description_valid);
 }
 
 close($logfh);
